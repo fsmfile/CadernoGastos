bfbb78e3943dc7833536ccd67930e4bb1ee70900
fsmfile <fsmfile@gmail.com>
2026-05-28 14:07:03 -0300

    alteração de layout

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // - Google Fonts: stale-while-revalidate
 // - Supabase e CDNs externos: não interceptados
 
-const VERSION = 'v1.8.1';
+const VERSION = 'v1.8.4';
 const CACHE_NAME = `caderno-gastos-${VERSION}`;
 
 const STATIC_SHELL = [
